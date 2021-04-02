d9f78f600b1c124b8c9f7d255a768f8bcd4acd3e
ruphys <benoitkagambega@gmail.com>
2021-04-02 11:58:39 +0000

    troisieme

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: vendor/bin/heroku-php-appache2 public/
-release: php artisan migrate:fresh --seed --force && php artisan && db:seed RoleSeeder
+release: php artisan migrate:fresh --seed --force && php artisan && db:seed RoleSeeder 
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Url;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Url::forceScheme('https');
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
     }
